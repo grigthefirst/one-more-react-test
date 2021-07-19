e010b60f77b52038de616b255cb3a57c40b6cd57
Gregory Syomin <gregory.syomin@nordigy.ru>
2021-07-19 15:24:50 +0300

    lint

--- algorithms/src/nthFibonacciNumber.ts
+++ algorithms/src/nthFibonacciNumber.ts
@@ -5,15 +5,14 @@ export function nthFibonacciNumber(n: number): number {
     return 1
   }
 
-  let previous = 0;
-  let current = 1;
+  let previous = 0
+  let current = 1
 
   for (let i = 2; i <= n; i++) {
-    const newCurrent = previous + current;
-    previous = current;
-    current = newCurrent;
+    const newCurrent = previous + current
+    previous = current
+    current = newCurrent
   }
 
-  return current;
+  return current
 }
-
